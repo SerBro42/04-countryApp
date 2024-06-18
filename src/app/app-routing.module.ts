591150f9fba4251af8048b3058a59b35e772f309
Sergey Brovko <serbro7@gmail.com>
2024-06-18 15:42:36 +0200

    routing module. Creation of home page component and about page component, with a shared module. Application routing configuration, with 2 URLs

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+
+/* Creamos este archivo con el propósito de guardar las rutas y URLs de la aplicación. Será nuesto
+módulo especializado en navegación. El comodín '**' significa que en caso de no tener nada en
+el URL, se redirige a 'home' */
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+/* El tipo de import que hagamos dependerá de si este es nuestro módulo de rutas principal o uno
+secundario. Si es el principal (como es nuestro caso), se usa forRoot. De lo contrario, se usa
+forChild. Pasamos como parámetro la definición de nuestras rutas (el array de objetos) */
+@NgModule({
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
