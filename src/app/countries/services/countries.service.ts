3fd0e5f9464ade5b4c227dcc317e898214cca971
Sergey Brovko <serbro7@gmail.com>
2024-06-20 18:24:22 +0200

    Error handling in case of incorrect value. RXJS pipe.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -14,11 +14,20 @@ export class CountriesService {
 
   /* La función searchCapital usa un HttpClient para conectarse con la API restcountries y
    devuelve un observable en forma de array de países */
+  /* Usamos el método .pipe propio de rxjs para hacer diversas operaciones con los datos del
+  observable, como por ejemplo tap() y map(). Los operadores se ejecutan en el mismo orden en
+  que aparecen enumerados dentro de .pipe */
+  /* Ponemos en el pipe un catchError. El operador "of" devuelve un array vacío en caso de que
+  se introduzca un valor incorrecto en la barra de búsqueda. De este modo, si insertamos un valor
+  correcto y acto seguido uno incorrecto, nos sale el mensaje "No hay países que mostrar" */
   searchCapital( searchTerm: string ): Observable<Country[]> {
 
     const url = `${ this.apiUrl }/capital/${ searchTerm }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([]) )
+      );
 
   }
 
